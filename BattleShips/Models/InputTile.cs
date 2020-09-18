a64476b72904e4e6baac06bbc3ee4947112339f5
Reece Gordon <reece.gordon.96@outlook.com>
2020-09-18 14:21:38 +0100

    User error message for X & Y was reversed, now displays as intended

--- BattleShips/Models/InputTile.cs
+++ BattleShips/Models/InputTile.cs
@@ -10,12 +10,12 @@ namespace BattleShips.Models
     public class InputTile
     {
         [Required]
-        [Range(1, 10)]
-        public int Y { get; set; }
+        [Range(1, 10, ErrorMessage = "The field X must be a number between 1 - 10.")]
+        public int X { get; set; }
 
         [Required]
         [StringLength(1)]
-        [RegularExpression(@"^([A-J])$", ErrorMessage ="The field X must be a character between A - J.")]
-        public string X { get; set; }
+        [RegularExpression(@"^([A-J])$", ErrorMessage ="The field Y must be a character between A - J.")]
+        public string Y { get; set; }
     }
 }
--- BattleShips/Models/Tile.cs
+++ BattleShips/Models/Tile.cs
@@ -7,8 +7,8 @@ namespace BattleShips.Models
 {
     public class Tile
     {
-        public char X { get; set; }
-        public int Y { get; set; }
+        public char Y { get; set; }
+        public int X { get; set; }
         public int ShipSpace { get; set; } //0 == No ship, 1 == BattleShip, 2 == Duncan, 3 == Defender, 4 == Hit
     }
 }
